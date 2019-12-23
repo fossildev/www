d0fe5990f720f2b95b7f51f12d80ea08364c23bd
Ahmad Nurus S <prksu.sh@gmail.com>
2019-12-23 17:51:19 +0700

    Add go import meta

--- src/components/CustomHelmet.js
+++ src/components/CustomHelmet.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Helmet from 'react-helmet';
+import { Location } from '@reach/router'
 
-
-const CustomHelmet = ({ page, image }) => (
+const CustomHelmet = ({ location, page, image }) => (
     <Helmet>
         <title>{page.title}</title>
         <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+        />
+        <meta
+            name="go-import"
+            content={`${location.host}${location.pathname} git https://github.com/fossildev${location.pathname}`}
         />
         <meta name="twitter:site" content="@sosc" />
         <meta property="og:type" content="website" />
@@ -26,5 +30,15 @@ const CustomHelmet = ({ page, image }) => (
     </Helmet>
 )
 
+const WithLocation = ComponentToWrap => props => (
+    <Location>
+        {({ location }) => (
+            <ComponentToWrap
+                {...props}
+                location={location}
+            />
+        )}
+    </Location>
+)
 
-export default CustomHelmet
+export default WithLocation(CustomHelmet)
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Layout from '../components/indexLayout';
+import GatsbyConfig from '../../gatsby-config'
+import CustomHelmet from '../components/CustomHelmet';
 
 const NotFoundPage = () => (
   <Layout>
+    <CustomHelmet page={GatsbyConfig.siteMetadata} />
     <div className="page">
     <div className="container">
       <h1>NOT FOUND</h1>
